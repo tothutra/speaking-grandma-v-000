f1200a9a47a2c1a1cf86d3fb3254ff87a940e6fc
Tra To <tothutra@gmail.com>
2018-08-11 03:48:57 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -12,6 +12,8 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
-
+def speak_to_grandma(isay)
+  if isay = "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+  elsif 
 end
